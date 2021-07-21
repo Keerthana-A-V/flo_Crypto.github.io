4de31468ea869f6bc9b882b601f1eb75229dae75
Keerthana-A-V <avkeerthanakeerthana78@gmail.com>
2021-07-21 22:29:06 +0530

    Shamirs secret

---
+++ js/plus.js
@@ -0,0 +1,15 @@
+
+
+function pluss()
+{
+const btnadd=document.getElementsByClassName("button");
+btnadd.addEventListener("click",addnew);
+                
+function addnew()
+{
+    const newdiv=document.createElement("div");
+    console.log("add");
+}
+}
+
+
